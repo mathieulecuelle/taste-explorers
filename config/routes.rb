e5040d6249a8c3e74cd6dc16d4f702402f457706
guillaumeautret <guillaume.autret@gmail.com>
2024-11-26 11:49:14 +0100

    routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -14,37 +14,26 @@ Rails.application.routes.draw do
       get :proposals
     end
 
-  #   member do
-  #     get :invite
-  #     get :memo
-  #   end
+    member do
+      get :invite
+      get :memo
+    end
 
     # Nested resources
     resources :bookings, only: [:new, :create]
     resources :questions, only: [:create]
   end
 
-  # Bookings routes
-  resources :bookings, only: [:index, :show] # Inclure `:show` ici pour simplifier
 
   # Dashboard routes
+  get "/dashboard", to: "bookings#index"
+  
   namespace :dashboard do
     resources :bookings, only: [:show]
     resources :meals, only: [:create] # Crée une route claire pour les meals
   end
 
+
   # User profile routes
   resource :user, only: [:edit, :update]
-
-  #   resources :bookings, only: [:new, :create]
-  #   resources :questions, only: [:create]
-  # end
-
-  # # Bookings routes
-  # resources :bookings, only: [:index]
-
-  # # Dashboard routes
-  # get "/dashboard", to: "bookings#index", as: "dashboard"
-  # post "/dashboard", to: "meals#create", as: "dashboard_create_meal"
-  # get "/dashboard/bookings/:id", to: "bookings#show", as: "dashboard_booking"
 end
