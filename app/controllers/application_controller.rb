6e033a4ba9f74603d6b764c431ff2c877f08c8ca
Mathieu Lecuelle <lecuelle.mathieu@gmail.com>
2024-12-03 15:11:38 +0100

    config again & again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
   protected
 
   # Permet à Devise de prendre en charge le champ `photo`
